2e91eea0b50d908b68549fb1d5ea8130d9051426
BritnyLain <bejaylain@gmail.com>
2022-12-01 12:04:53 -0600

    added images to hero component

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./HeroSection.css";
-
+import lawnCare from "../../images/lawn-care.jpg";
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="hero-container">
+      <img className="hero-img" src={lawnCare} alt="groomed lawn care" />
+    </div>
+  );
 };
 
 export default HeroSection;
